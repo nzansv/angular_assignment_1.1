cd72236d9e4e696aa79637134b221000a324c9be
nzansv <63594033+nzansv@users.noreply.github.com>
2021-02-04 15:05:33 +0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ResultsComponent } from './results/results.component';
 import { AddRecordComponent } from './add-record/add-record.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -14,7 +14,8 @@ import { AddRecordComponent } from './add-record/add-record.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
